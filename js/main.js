c774f4afff7d31223257513aa16c70dd78755408
Elsayedmostafa574 <alsayedb574@gmail.com>
2023-01-18 18:01:46 +0200

    Elsayed

--- js/main.js
+++ js/main.js
@@ -4,18 +4,3 @@ var typed = new Typed(".about", {
   ],
   typeSpeed: 50,
 });
-
-$(document).ready(function () {
-  $(window).scroll(function () {
-    let x = $(window).scrollTop();
-    if (x > 600) {
-      $(".navbar").css({
-        background: "#53A0E8",
-      });
-    } else {
-      $(".navbar").css({
-        background: "transparent",
-      });
-    }
-  });
-});
